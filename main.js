598e488adf5dee5eb05b99afd7923a5e0a1c7441
Lake Heckaman <lakeheckaman@gmail.com>
2022-12-05 18:37:49 -0500

    tt

--- main.js
+++ main.js
@@ -248,9 +248,9 @@ let sunraysTemp;
 let noise;
 let input;
 //load texture for dithering
-let ditheringTexture = createTextureAsync('LDR_LLL1_0.png');
-let picture = createTextureAsync('img/flowers_fence.JPG');
-// let picture = createTextureAsync('img/lake-heckaman-IMG_0997-dec-2022.jpg');
+let ditheringTexture = LGL.createTextureAsync('LDR_LLL1_0.png');
+let picture = LGL.createTextureAsync('img/flowers_fence.JPG');
+// let picture = LGL.createTextureAsync('img/lake-heckaman-IMG_0997-dec-2022.jpg');
 // console.log('loaded picture successfully');
 
 //create all our shader programs 
@@ -359,47 +359,6 @@ function initSunraysFramebuffers () {
     sunraysTemp = LGL.createFBO(res.width, res.height, r.internalFormat, r.format, texType, filtering);
 }
 
-
-
-function createTextureAsync (url) {
-    let texture = gl.createTexture();
-    gl.bindTexture(gl.TEXTURE_2D, texture);
-    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
-    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.LINEAR);
-    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.REPEAT);
-    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.REPEAT);
-    gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGB, 1, 1, 0, gl.RGB, gl.UNSIGNED_BYTE, new Uint8Array([255, 255, 255]));
-
-    let obj = {
-        texture,
-        width: 1,
-        height: 1,
-        attach (id) {
-            gl.activeTexture(gl.TEXTURE0 + id);
-            gl.bindTexture(gl.TEXTURE_2D, texture);
-            return id;
-        }
-    };
-
-    let image = new Image();
-    image.onload = () => {
-        obj.width = image.width;
-        obj.height = image.height;
-        gl.bindTexture(gl.TEXTURE_2D, texture);
-        gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGB, gl.RGB, gl.UNSIGNED_BYTE, image);
-    };
-    image.src = url;
-
-    return obj;
-}
-
-// function LGL.updateKeywords () {
-//     let displayKeywords = [];
-//     if (config.SHADING) displayKeywords.push("SHADING");
-//     if (config.BLOOM) displayKeywords.push("BLOOM");
-//     if (config.SUNRAYS) displayKeywords.push("SUNRAYS");
-//     displayMaterial.setKeywords(displayKeywords);
-// }
 
 
 // Draw the scene.
